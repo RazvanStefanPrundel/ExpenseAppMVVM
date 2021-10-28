862c48d56f7e3cbda697b7f489a4898d388de5d3
New user <ionut.doroftei@assist.ro>
2021-10-28 16:19:47 +0300

    Add BudgetFragment functionalities

--- app/src/main/java/com/example/expenseappmvvm/screen/mainScreen/budgetScreen/BudgetFragment.kt
+++ app/src/main/java/com/example/expenseappmvvm/screen/mainScreen/budgetScreen/BudgetFragment.kt
@@ -1,10 +1,17 @@
 package com.example.expenseappmvvm.screen.mainScreen.budgetScreen
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.expenseappmvvm.R
 import com.example.expenseappmvvm.databinding.FragmentBudgetBinding
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import kotlinx.android.synthetic.main.fragment_budget.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class BudgetFragment : Fragment() {
@@ -24,12 +31,45 @@ class BudgetFragment : Fragment() {
         initObservers()
     }
 
-    fun initComponents(){
+    private fun initComponents(){
         budgetViewModel.initAmounts()
+        setBarChart()
+
+        if (budgetViewModel.currentBalance.value!! < 0.0) {
+            ll_current_balance.setBackgroundResource(R.drawable.border_current_balance_negative)
+            tv_current_balance_txt.setTextColor(resources.getColor(R.color.red))
+            tv_current_balance.setTextColor(resources.getColor(R.color.red))
+        } else {
+            ll_current_balance.setBackgroundResource(R.drawable.border_current_balance_positive)
+            tv_current_balance_txt.setTextColor(resources.getColor(R.color.green))
+            tv_current_balance.setTextColor(resources.getColor(R.color.green))
+        }
     }
 
-    fun initObservers(){
+    private fun initObservers(){
 
+    }
+
+    private fun setBarChart() {
+
+        budgetViewModel.initBarChartData()
+
+        val barDataSetP = BarDataSet(budgetViewModel.barsEntriesP, "")
+        val barDataSetN = BarDataSet(budgetViewModel.barsEntriesN, "")
+
+        barDataSetN.setColors(Color.RED)
+        barDataSetP.setColors(Color.GREEN)
+        val barData = BarData()
+        barData.addDataSet(barDataSetP)
+        barData.addDataSet(barDataSetN)
+        chart_budget_details.animateY(1500)
+
+        val xAxisLabels = listOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+        chart_budget_details.xAxis.valueFormatter = IndexAxisValueFormatter(xAxisLabels)
+        chart_budget_details.xAxis.position = XAxis.XAxisPosition.BOTH_SIDED
+
+        chart_budget_details.data = barData
+        chart_budget_details.invalidate()
     }
 
     companion object {
--- app/src/main/java/com/example/expenseappmvvm/screen/mainScreen/budgetScreen/BudgetViewModel.kt
+++ app/src/main/java/com/example/expenseappmvvm/screen/mainScreen/budgetScreen/BudgetViewModel.kt
@@ -7,6 +7,8 @@ import com.example.expenseappmvvm.data.database.prefs.PreferencesProvider
 import com.example.expenseappmvvm.data.database.repositories.ExpenseRepository
 import com.example.expenseappmvvm.utils.rxUtils.AppRxSchedulers
 import com.example.expenseappmvvm.utils.rxUtils.disposeBy
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import io.reactivex.disposables.CompositeDisposable
 import timber.log.Timber
 import java.util.*
@@ -17,29 +19,39 @@ class BudgetViewModel(
     private val rxSchedulers: AppRxSchedulers,
     private val prefs: PreferencesProvider
 ) : ViewModel() {
+    val barsEntriesP: ArrayList<BarEntry> = ArrayList()
+    val barsEntriesN: ArrayList<BarEntry> = ArrayList()
 
     val expense = MutableLiveData<Expense>().apply { value = Expense() }
 
-    var currentBalance = MutableLiveData<String>().apply { value = "0.0" }
+    var todayExpense = MutableLiveData<Double>().apply { value = 0.0 }
+    var weekExpense = MutableLiveData<Double>().apply { value = 0.0 }
+    var monthExpense = MutableLiveData<Double>().apply { value = 0.0 }
 
-    var todayExpense = MutableLiveData<String>().apply { value = "0.0" }
-    var weekExpense = MutableLiveData<String>().apply { value = "0.0" }
-    var monthExpense = MutableLiveData<String>().apply { value = "0.0" }
+    var currentBalance = MutableLiveData<Double>().apply { value = 0.0 }
+
+    private val userId = prefs.getUserId()!!
 
     private val cal = Calendar.getInstance()
-    private val userId = prefs.getUserId()!!
+
+    private val sumOfMonth = MutableList(12) { 0.0 }
+
+    private var sumOfMonthAmount = MutableLiveData<Double>().apply { value = 0.0 }
+
+    private val barDataSetP = MutableLiveData<BarDataSet>().apply { value = BarDataSet(barsEntriesP, "") }
+    private val barDataSetN = MutableLiveData<BarDataSet>().apply { value = BarDataSet(barsEntriesN, "") }
 
     fun initAmounts() {
         expense.value?.let {
             expenseRepository.getCurrentBalance(userId)
                 .observeOn(rxSchedulers.androidUI())
                 .subscribe({
-                    currentBalance.value = it.toString()
+                    currentBalance.value = it
                 }, {
                     Timber.e(it.localizedMessage)
                 })
                 .disposeBy(compositeDisposable)
-        }.toString()
+        }
 
         cal.set(Calendar.HOUR_OF_DAY, 1)
         val today = cal.timeInMillis
@@ -52,35 +64,68 @@ class BudgetViewModel(
             expenseRepository.getExpensesAmount(today, userId)
                 .observeOn(rxSchedulers.androidUI())
                 .subscribe({
-                    todayExpense.value = it.toString()
+                    todayExpense.value = it
                 }, {
                     Timber.e(it.localizedMessage)
                 })
                 .disposeBy(compositeDisposable)
-        }.toString()
+        }
 
         expense.value?.let {
             expenseRepository.getExpensesAmount(week, userId)
                 .observeOn(rxSchedulers.androidUI())
                 .subscribe({
-                    weekExpense.value = it.toString()
+                    weekExpense.value = it
                 }, {
                     Timber.e(it.localizedMessage)
                 })
                 .disposeBy(compositeDisposable)
-        }.toString()
+        }
 
         expense.value?.let {
             expenseRepository.getExpensesAmount(month, userId)
                 .observeOn(rxSchedulers.androidUI())
                 .subscribe({
-                    monthExpense.value = it.toString()
+                    monthExpense.value = it
                 }, {
                     Timber.e(it.localizedMessage)
                 })
                 .disposeBy(compositeDisposable)
-        }.toString()
+        }
+    }
 
+    fun initBarChartData() {
+        for (i in 0..11) {
+            val firstDayOfMonth = getFirstDayOfMonth(i)
+            val firstDayOfNextMonth = getFirstDayOfMonth(i + 1)
+            sumOfMonthAmount.value?.let {
+                expenseRepository.getExpenseFromTo(firstDayOfMonth, firstDayOfNextMonth, userId)
+                    .observeOn(rxSchedulers.androidUI())
+                    .subscribe({
+                        sumOfMonth[i] = it
+                    }, {
+                        Timber.e(it.localizedMessage)
+                    }).disposeBy(compositeDisposable)
+                sumOfMonth[i]
+
+                if (sumOfMonth[i] < 0.0) {
+                    barsEntriesN.add(BarEntry(i.toFloat(), sumOfMonth[i].toFloat()))
+                    barDataSetN.value = BarDataSet(barsEntriesN, "Negative")
+                } else {
+                    barsEntriesP.add(BarEntry(i.toFloat(), sumOfMonth[i].toFloat()))
+                    barDataSetP.value = BarDataSet(barsEntriesP, "Positive")
+                }
+            }
+        }
+    }
+
+    private fun getFirstDayOfMonth(monthOfYear: Int): Long {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.MONTH, monthOfYear)
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        return calendar.timeInMillis
     }
 
 }
